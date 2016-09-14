f3661dd4cbb0978cb271f56b0f40fce7a6c11a84
map-lijie <15695889613@163.com>
2016-09-14 22:59:42 +0800

    asdASDADAS

--- src/main/java/com/lijie/example/ArrayTest.java
+++ src/main/java/com/lijie/example/ArrayTest.java
@@ -4,6 +4,8 @@ package com.lijie.example;
 public class ArrayTest {
 
 	public static void main(String[] args) {
+		
+		
           int []a ={1,2,3,4,5,6,7};
 		System.arraycopy(a, 4, a, 0, 3);
 		StringBuilder sb =new  StringBuilder();
